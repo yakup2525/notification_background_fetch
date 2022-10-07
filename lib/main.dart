89212b0e485dabcc6a3ec5d43323dc1ab4cb47c0
Yakup <yakup_okumus@outlook.com>
2022-10-08 02:47:15 +0300

    Background fetch added

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'notification.dart';
 
+import 'dart:async';
 
+import 'package:flutter/services.dart';
+
+import 'package:background_fetch/background_fetch.dart';
 
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -15,6 +19,7 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterL
 
 void main() {
   runApp(const MyApp());
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
 }
 
 
@@ -46,37 +51,111 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-
-
+  bool _enabled = true;
+  int _status = 0;
+Timer? timer;
 @override
   void initState() {
    
     super.initState();
-        Notif.initialize(flutterLocalNotificationsPlugin);
+    Notif.initialize(flutterLocalNotificationsPlugin);
+    initPlatformState();
+  }
+
+
+// Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    // Configure BackgroundFetch.
+    int status = await BackgroundFetch.configure(BackgroundFetchConfig(
+        minimumFetchInterval: 15,
+        stopOnTerminate: false,
+        enableHeadless: true,
+        requiresBatteryNotLow: false,
+        requiresCharging: false,
+        requiresStorageNotLow: false,
+        requiresDeviceIdle: false,
+        requiredNetworkType: NetworkType.NONE
+    ), (String taskId) async {  // <-- Event handler
+      // This is the fetch-event callback.
+      print("[BackgroundFetch] Event received $taskId");
+    
+Timer.periodic(const Duration(seconds: 15), (timer) {
+  Notif.showBigTextNotification(title: "New message title", body: "Your long body", fln: flutterLocalNotificationsPlugin);
+              
+});
+  
+
+      // IMPORTANT:  You must signal completion of your task or the OS can punish your app
+      // for taking too long in the background.
+      BackgroundFetch.finish(taskId);
+    }, (String taskId) async {  // <-- Task timeout handler.
+      // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
+      print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
+      BackgroundFetch.finish(taskId);
+    });
+    print('[BackgroundFetch] configure success: $status');
+    setState(() {
+      _status = status;
+    });        
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+  }
+void _onClickEnable(enabled) {
+    setState(() {
+      _enabled = enabled;
+    });
+    if (enabled) {
+      BackgroundFetch.start().then((int status) {
+        print('[BackgroundFetch] start success: $status');
+      }).catchError((e) {
+        print('[BackgroundFetch] start FAILURE: $e');
+      });
+    } else {
+      BackgroundFetch.stop().then((int status) {
+        print('[BackgroundFetch] stop success: $status');
+      });
+    }
+  }
+
+  void _onClickStatus() async {
+    int status = await BackgroundFetch.status;
+    print('[BackgroundFetch] status: $status');
+    setState(() {
+      _status = status;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-      
-        title: Text(widget.title),
-      ),
+      appBar:AppBar(
+          title: const Text('BackgroundFetch ', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.amberAccent,
+         
+          actions: <Widget>[
+            Switch(value: _enabled, onChanged: _onClickEnable),
+          ]
+        ),
       body:  Padding(
         padding:  const EdgeInsets.all(40.0),
         child:  Column(
           children: [
-            Center(
-            child: ElevatedButton(
+            ElevatedButton(
               onPressed: (){
                 Notif.showBigTextNotification(title: "New message title", body: "Your long body", fln: flutterLocalNotificationsPlugin);
               
               },
               child: const Text('Notification Gönder'),),
-     
-            ),
       
+       ElevatedButton(
+              onPressed: 
+               _onClickStatus
+              ,
+              child:Text("$_status"),),
 
            
           ],
@@ -85,4 +164,22 @@ class _MyHomePageState extends State<MyHomePage> {
    
     );
   }
+}
+
+
+// [Android-only] This "Headless Task" is run when the Android app 
+// is terminated with enableHeadless: true
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  String taskId = task.taskId;
+  bool isTimeout = task.timeout;
+  if (isTimeout) {
+    // This task has exceeded its allowed running-time.  
+    // You must stop what you're doing and immediately .finish(taskId)
+    print("[BackgroundFetch] Headless task timed-out: $taskId");
+    BackgroundFetch.finish(taskId);
+    return;
+  }  
+  print('[BackgroundFetch] Headless event received.');
+  // Do your work here...
+  BackgroundFetch.finish(taskId);
 }
